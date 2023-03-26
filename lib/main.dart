282c20a2f4dc09351f470737d5cade9d36d45984
Wallace Almeida <contato@programeparaandroid.com.br>
2023-03-26 15:33:31 -0300

    - Atualização para resolver problema deixado no commit anterior.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vetadvisor/prelogin/criarConta.dart';
 import 'package:vetadvisor/fluxoprontuariodigital/perfil.dart';
 import 'package:vetadvisor/prelogin/login.dart';
-import 'package:vetadvisor/recursos/firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  runApp(const CriarConta());
+main()  {
+  runApp(const Login());
 }
 
 

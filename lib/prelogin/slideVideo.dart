f41cbaadd2523f1300fb7c57e595e75ff4befcff
Wallace Almeida <wallace_sjm@msn.com>
2023-04-04 15:18:39 -0300

    - Correções de layout e processamento na tela logado, gerando os movimentos necessários.

--- lib/prelogin/slideVideo.dart
+++ lib/prelogin/slideVideo.dart
@@ -17,36 +17,6 @@ class SlideVideo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 1000),
-      curve: Curves.easeOutQuint,
-
-      /*decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-              image: AssetImage(this.video), //chamo as imagens aqui
-              //para a imagem ocupar toda tela
-              fit: BoxFit.cover),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black87,
-            )
-          ]),*/
-      //color: Colors.blue,
-      child: FutureBuilder(
-          future: _initializeVideoPlayerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return AspectRatio(
-                aspectRatio: _controllerV.value.aspectRatio,
-                child: VideoPlayer(_controllerV),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }),
-    );
+    return Container();
   }
 }
--- lib/prelogin/slide_tile.dart
+++ lib/prelogin/slide_tile.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:vetadvisor/prelogin/logado.dart';
 
@@ -14,30 +15,56 @@ class SlideTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  AnimatedContainer(
+    int _indexCarrousel = 0;
+    return  CarouselSlider(
 
-      duration: Duration(milliseconds: 1000),
-      curve: Curves.easeOutQuint,
-     // margin: EdgeInsets.all(10),
-       // margin: EdgeInsets.only( bottom: 100, right: 30),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        image: DecorationImage(
+        items: [
+          AnimatedContainer(
 
-          image: AssetImage(this.image), //chamo as imagens aqui
-          //para a imagem ocupar toda tela
-          fit: BoxFit.cover
-        ),
-          boxShadow: [
-          BoxShadow(
-          color: Colors.black87,
-          //blurRadius: blur,
-         // offset: Offset(offset, offset)
+              duration: const Duration(milliseconds: 1000),
+              curve: Curves.easeOutQuint,
+              // margin: EdgeInsets.all(10),
+              // margin: EdgeInsets.only( bottom: 100, right: 30),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+
+                      image: AssetImage(this.image), //chamo as imagens aqui
+                      //para a imagem ocupar toda tela
+                      fit: BoxFit.cover
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black87,
+                      //blurRadius: blur,
+                      // offset: Offset(offset, offset)
+                    )
+                  ]
+              )
+              //color: Colors.blue,
+
           )
-        ]
-      )
-        //color: Colors.blue,
-
+          ], options: CarouselOptions(
+      height: 120,
+      onPageChanged: (index, resource) {
+        _indexCarrousel++;
+        print(_indexCarrousel);
+      },
+      // aspectRatio: 16 / 9,
+      //viewportFraction: 0.8,
+      //initialPage: 0,
+      //enableInfiniteScroll: true,
+      //reverse: false,
+      autoPlay: true,
+      autoPlayInterval: Duration(seconds: 4),
+      autoPlayAnimationDuration:
+      Duration(milliseconds: 800),
+      autoPlayCurve: Curves.fastOutSlowIn,
+      enlargeCenterPage: true,
+      // enlargeFactor: 0.3,
+      //onPageChanged: callbackFunction,
+      scrollDirection: Axis.horizontal,
+    ),
     );
   }
 
